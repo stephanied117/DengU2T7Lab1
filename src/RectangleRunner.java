41fd0198f4ed6588097b69f8486bd74dc528b83c
BT_1N3_06 <sample@bths.edu>
2023-10-05 11:06:37 -0400

    initial commit

---
+++ src/RectangleRunner.java
@@ -0,0 +1,9 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+        String blah2 = "What's for dinner?";
+        String ugh2 = blah2.substring(11);
+        System.out.println(ugh2);
+        String ugh3 = blah2.substring(11,18);
+        System.out.println(ugh3);
+    }
+}
